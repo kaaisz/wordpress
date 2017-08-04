de2c70ef1955b429e6173c56b936a5d1ef577aa8
SuzukiKaai <suzy092@gmail.com>
2017-08-05 01:12:50 +0900

    navの位置調整、「メニュー」文字消去

---
+++ wp-content/themes/szdp-2017/template-parts/navigation/navigation-top.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Displays top navigation
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'twentyseventeen' ); ?>">
+	<img class="logo-nav" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/logo.svg"></img>
+	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
+		<?php
+		echo twentyseventeen_get_svg( array( 'icon' => 'bars' ) );
+		echo twentyseventeen_get_svg( array( 'icon' => 'close' ) );
+		// _e( 'Menu', 'twentyseventeen' );
+		?>
+		<!-- <span></span> -->
+		<!-- <span></span> -->
+		<!-- <span></span> -->
+	</button>
+
+	<?php wp_nav_menu( array(
+		'theme_location' => 'top',
+		'menu_id'        => 'top-menu',
+	) ); ?>
+
+	<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && has_custom_header() ) : ?>
+		<a href="#content" class="menu-scroll-down">
+			<?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?>
+			<span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?>
+			</span>
+		</a>
+	<?php endif; ?>
+
+</nav><!-- #site-navigation -->
